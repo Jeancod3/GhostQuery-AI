f4917772f88f12da1508f88b081538f7db7e5bc9
Ghost Max <jeanlozanox@gmail.com>
2026-06-05 17:01:57 -0500

    Eliminado el archivo bat ya que solo usaré la versión en la nube

--- iniciar_ghostquery.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title GhostQuery AI - Lanzador
-echo ===========================================
-echo      Iniciando GhostQuery AI...
-echo ===========================================
-echo.
-echo Comprobando entorno...
-call streamlit run app.py
-pause
